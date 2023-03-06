7153f3505dfc9425519a8ad46ab80846406cbff6
Artem <artfed@ttu.ee>
2023-03-06 22:18:34 +0200

    added all the necessary input lines, but i still have to separate names into different inputs

--- index.php
+++ index.php
@@ -22,18 +22,27 @@
     </header>
     <main>
         <form action="form.validation.html" method="post" id="data">
+            <!-- Separate lines for last first and middle names-->
             <label for="name">Name:</label>
             <input type="text" id="name" required placeholder="Enter your first, last and middle(optional) names"><br>
             <label for="salutation">Salutation:</label>
             <select id="salutation">
-                <option value="">--Please choose your salutation--</option>
+                <option value="">--Please choose your salutation--</option><br>
                 <option value="mr">Mr</option>
                 <option value="ms">Ms</option>
                 <option value="mrs">Mrs</option>
                 <option value="sir">Sir</option>
                 <option value="doctor">Doctor</option>
                 <option value="other">Other</option>
-            </select>
+            </select><br>
+            <label for="age">Age:</label>
+            <input type="number" id="age" name="age" size="7" min="18" max="99" value="21" required><br>
+            <label for="email">e-mail:</label>
+            <input type="email" name="email" id="email" required placeholder="Enter a valid email address"><br>
+            <label for="phone">Phone:</label>
+            <input type="tel" id="phone" name="phone" pattern="[0-9,+]{1,6} [0,9]{3} [0-9]{3}"required placeholder="Number as 000 000 000" readonly><br>
+            <label for="arrival">Date of arrival:</label>
+            <input type="date" name="arrival" id="arrival" min="2023-01-01" max="2033-01-01" required><br>
         </form>
     </main>
 </body>
