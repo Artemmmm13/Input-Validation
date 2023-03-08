34312edb96fcd1c912d9f7903d158d69b39d2baf
Artem <artfed@ttu.ee>
2023-03-08 10:29:49 +0200

    added proper id and names for inputs

--- index.php
+++ index.php
@@ -1,28 +1,29 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         // receiving data from user
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $mname = $_POST['mname'];
-        $salutation = $_POST['salutation'];
+        $fname = $_POST['nameFirst'];
+        $lname = $_POST['nameLast'];
+        $mname = $_POST['nameMiddle'];
+        $salutation = $_POST['saluteMr,saluteMs,saluteOther'];
         $age = $_POST['age'];
         $email = $_POST['email'];
         $phone = $_POST['phone'];
-        $date = $_POST['arrival'];
-        $agree = $_POST['agree'];
+        $date = $_POST['dateArrive'];
+        $comment = $_POST['comment'];
+        $submit = $_POST['submitReservation'];
         $startDate = new DateTime("2023-01-01");
         $endDate = new DateTime("2033-01-01");
         $userDate = new DateTime($date);
 
         // validation of data
         $issues = [];
-        if (!isset($fname)){
+        if (empty($fname)){
             $issues[] = "First name is required";
         }
         if (!preg_match("/^[a-zA-z]*$/", $fname)){
             $issues[] = "Your first name contains invalid symbols";
         }
-        if (!isset($lname)){
+        if (empty($lname)){
             $issues[] = "Last name is required";
         }
         if (!preg_match("/^[a-zA-z]*$/", $lname)){
@@ -31,62 +32,35 @@
         if (!preg_match("/^[a-zA-z]*$/", $mname)){
             $issues[] = "Your middle name contains invalid symbols";
         }
-        if (!isset($salutation)){
+        if (empty($salutation)){
             $issues[] = "Salutation is required";
         }
-        if (!isset($age)){
+        if (empty($age)){
             $issues[] = "Enter your age please";
         }
         if ($age > 99 || $age < 18){
             $issues[] = "Your age is in the invalid range";
         }
-        if (!isset($email)){
+        if (empty($email)){
             $issues[] = "Email is required";
         }
         if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^/", $email)){
             $issues[] = "Email address contains invalid symbols";
         }
-        if (!isset($phone)){
+        if (empty($phone)){
             $issues[] = "Phone number is required";
         }
         if (!preg_match("/^[0-9]*$/", $phone)){
             $issues[] = "Only numeric values in number are allowed";
         }
-        if (!isset($date)){
+        if (empty($date)){
             $issues[] = "Arrival date is required";
         }
         if ($userDate > $endDate || $userDate < $startDate){
             $issues[] = "Your date of arrival is not in the given range";
         }
-        if (!isset($agree)){
+        if (empty($agree)){
             $issues[] = "Please confirm that you have agreed to our terms and conditions";
-        }
-
-        if (empty($issues) && !empty($_POST)){
-             // otherwise writing user data into csv file
-           $fileName = 'data.csv';
-
-           // checking if such a file exists
-           if (!file_exists($fileName)){
-            $file = fopen($fileName, 'w');
-            fputcsv($file, array('First Name', 'Middle Name', 'Last Name', 'Salutation', 'Age', 'Email', 'Phone', 'Date'), ';');
-            fclose($file);
-           }
-
-           // open existing file and writing user data into it
-           try{
-            $file = fopen($fileName, 'a');
-            fputcsv($file, array($_POST['fname'], $_POST['mname'], $_POST['lname'], $_POST['salutation'], $_POST['age'], $_POST['email'], $_POST['phone'], $_POST['arrival']), ';');
-            echo end(file($fileName));
-            fclose($file);
-           } catch (Exception $e){
-            throw new Exception("File wasn't found!");
-           }
-        }
-        else{
-            foreach($issues as $issue){
-                echo "<p>$issue</p>";
-            }
         }
         }
 ?>
@@ -117,13 +91,13 @@
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" id="data">
             <!-- Separate lines for last first and middle names-->
             <label for="fname">First Name:</label>
-            <input type="text" id="fname" required placeholder="Enter your first name"><br>
+            <input type="text" name="nameFirst" id="nameFirst" required placeholder="Enter your first name"><br>
             <label for="lname">Last name:</label>
-            <input type="text" name="lname" id="lname" required placeholder="Enter your last name"><br>
+            <input type="text" name="nameLast" id="nameLast" required placeholder="Enter your last name"><br>
             <label for="mname">Middle name(optional)</label>
-            <input type="text" name="mname" id="mname" placeholder="Enter your middle name"><br>
+            <input type="text" name="nameMiddle" id="nameMMiddle" placeholder="Enter your middle name"><br>
             <label for="salutation">Salutation:</label>
-            <select id="salutation" required>
+            <select id="saluteMr,saluteMs,saluteOther" name="salute" required>
                 <option value="">--Please choose your salutation--</option><br>
                 <option value="mr">Mr</option>
                 <option value="ms">Ms</option>
@@ -139,11 +113,26 @@
             <label for="phone">Phone:</label>
             <input type="tel" id="phone" name="phone" pattern="[0-9,+]{1,6} [0,9]{3} [0-9]{3}" placeholder="Number as 000 000 000"><br>
             <label for="arrival">Date of arrival:</label>
-            <input type="date" name="arrival" id="arrival" min="2023-01-01" max="2033-01-01" required><br>
-            <input type="checkbox" id="agree" name="agree">
-            <label for="agree">I agree to terms and conditions</label><br>
-            <input type="submit" value="submit" required>
+            <input type="date" name="dateArrive" id="dateArrive" min="2023-01-01" max="2033-01-01" required><br>
+            <label for="comment">Comment:</label>
+            <textarea name="comment" id="comment" cols="30" rows="10"></textarea><br>
+            <label for="submit"></label>
+            <input type="submit" value="submit" name="submitReservation" id="submitReservation"required>
         </form>
+        <?php
+        if (!empty($issues)){
+            foreach($issues as $issue){
+                echo "<p>$issue</p>";
+            }
+        }
+        else{
+            $fileName = 'data.csv';
+            $file = fopen($fileName, 'a+');
+            $csvData = array($_POST['fname'], $_POST['mname'], $_POST['lname'], $_POST['salutation'], $_POST['age'], $_POST['email'], $_POST['phone'], $_POST['arrival'], $_POST['agree'], ';');
+            fputcsv($file, $csvData);
+            fclose($file);
+        }
+        ?>
     </main>
 </body>
 </html>
